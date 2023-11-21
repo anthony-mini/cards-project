92d4e7cd020225f411c755d5480b93f475095ff1
anthony-mini <anthony-mini@outlook.fr>
2023-11-21 12:47:14 +0100

    feat: add json and use aleatoire quest

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -3,10 +3,14 @@ import React, { useState, useEffect } from 'react';
 import QrCodeImg1 from '../../assets/image-qr-code-v1.png';
 import QrCodeImg2 from '../../assets/image-qr-code-v2.png';
 
+import quest from '../../json/quest.json';
+
 export default function CodeQr() {
   const [width, setWidth] = useState(window.innerWidth);
   const [isFlipped, setIsFlipped] = useState(false);
   const [image, setImage] = useState(width >= 750 ? QrCodeImg1 : QrCodeImg2);
+  const [randomQuest, setRandomQuest] = useState(null);
+  const [showAnswer, setShowAnswer] = useState(false);
 
   useEffect(() => {
     function handleWindowResize() {
@@ -16,6 +20,9 @@ export default function CodeQr() {
 
     window.addEventListener('resize', handleWindowResize);
 
+    // Chargement de la question aléatoire une seule fois
+    setRandomQuest(quest[Math.floor(Math.random() * quest.length)]);
+
     return () => {
       window.removeEventListener('resize', handleWindowResize);
     };
@@ -23,7 +30,10 @@ export default function CodeQr() {
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
+    setShowAnswer(!showAnswer); // Toggle l'affichage de la réponse
   };
+
+  if (!randomQuest) return <div>Loading...</div>;
 
   return (
     <>
@@ -31,20 +41,14 @@ export default function CodeQr() {
         <div className="container-card front">
           {/* Contenu de la face avant */}
           <img src={image} alt="qr-code" />
-          <h2> Front </h2>
-          <p>
-            Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level
-          </p>
+          <h2>{randomQuest.Titre}</h2>
+          <p>{randomQuest.Description}</p>
         </div>
         <div className="container-card back">
           {/* Contenu de la face arrière */}
           <img src={image} alt="qr-code" />
-          <h2> Back </h2>
-          <p>
-            Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level
-          </p>
+          <h2>{randomQuest.Titre}</h2>
+          {showAnswer && <p>Réponse: {randomQuest['Bonne Réponse']}</p>}
         </div>
       </div>
       <div className="container-button">
