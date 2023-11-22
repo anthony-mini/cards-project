b73ae7dccb24fb3150451f857ae67032bb70906a
anthony-mini <anthony-mini@outlook.fr>
2023-11-22 13:55:35 +0100

    s'style: Increase margin-top for container-button, adjust header style, remove position for backward-button'

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Logo from '../../assets/logo.svg';
 
 export default function Home() {
   return (
     <>
       <main>
+        <header className="header-container">
+          <img className="header-logo" src={Logo} alt="" />
+          <h1 className="header-title">The Designer's Odyssey</h1>
+        </header>
         <h1>Home</h1>
         <a href="/question">question</a> <br />
         <a href="/challenges">challenges</a>
